f46c4e03608909371952a7e9de1afafe93376545
nama <senapratama013@gmail.com>
2022-11-09 11:54:25 +0700

    first commit

--- Challenge-6/src/redux/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import rootReducers from "./reducers";
-
-const store = configureStore({
-  reducer: rootReducers,
-  devTools: process.env.NODE_ENV === 'development',
-});
-
-export default store;
